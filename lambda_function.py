85c892eaf65c9a2c3c88099c0db98f4fac78d1d2
Wes Ladd <wsladd@icloud.com>
2023-02-23 10:07:26 -0600

    updates to flask app to handle various routes and do allow listing on source_ip + user_agent - still need link handling using base_url

--- lambda_function.py
+++ lambda_function.py
@@ -1,57 +1,90 @@
 #!/usr/bin/env python3
 import urllib3
-import bs4
 import json
-from bs4 import BeautifulSoup
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
-
-# Import Flask
-from flask import Flask, request, jsonify, render_template
-app = Flask(__name__)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-# Disable the "Not Secure" warning when using HTTPS:
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-#Dummy event and context for local mocking
-# event = {'queryStringParameters':'', 'pathParameters':'', 'headers':'', 'body':'', 'requestContext':{'identity':{'sourceIp':'', 'userAgent':''}}}
-# context = {}
 
 # Initialize the AWS Lambda Powertools
 tracer = Tracer()
 logger = Logger()
 #app = APIGatewayRestResolver()
 
+
+# Import Flask
+from flask import Flask, request, jsonify, render_template
+app = Flask(__name__)
+
+# Define Flask routes
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/a')
+def index():
+    return render_template('index.html')
+
+
 #Uncomment the line below if you want to use the Lambda Powertools Logger
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event, context):
-    # parse event data for query string parameters, path parameters, headers, body, source IP and user agent
-    event_data = event['queryStringParameters']
-    path = event['path']
-    event_headers = event['headers']
-    event_body = event['body']
-    event_source_ip = event['requestContext']['identity']['sourceIp']
-    event_user_agent = event['requestContext']['identity']['userAgent']
-    # log the event data
-    logger.info(event_data)
+    # Analyze incoming HTTP Request, including path of requested resource.
+    http_path = event['path']
+    http_query_string_parameters = event['queryStringParameters']
     http_method = event['httpMethod']
-    if http_method == 'GET' and path == '/about':
-        with app.app_context():
-            html = render_template('about.html')
-        return {
-            "statusCode": 200,
-            "body": html,
-            "headers": {
-                'Content-Type': 'text/html',
+    http_headers = event['headers']
+    http_body = event['body']
+
+    # # parse event data for source IP and user agent
+    # event_source_ip = event['requestContext']['identity']['sourceIp']
+    # event_user_agent = event['requestContext']['identity']['userAgent']
+
+    # # IP Allow List
+    # if event_source_ip not in ['71.78.212.171']:
+    #     return {
+    #         "statusCode": 403,
+    #         "body": "Forbidden",
+    #         "headers": {
+    #             'Content-Type': 'text/html',
+    #         }
+    #     }
+    # else:
+    #     pass
+
+    # if event_user_agent not in ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36']:
+    #     return {
+    #         "statusCode": 403,
+    #         "body": "Forbidden",
+    #         "headers": {
+    #             'Content-Type': 'text/html',
+    #         }
+    #     }
+    
+    # Render the requested page
+    try:
+        if http_method == 'GET' :
+            with app.app_context():
+                if http_path == '/':
+                    html = render_template('index.html')
+                else:
+                    html = render_template(http_path[1:] + '.html')
+            return {
+                "statusCode": 200,
+                "body": html,
+                "headers": {
+                    'Content-Type': 'text/html',
+                }
             }
-        }
-    else:
+        else:
+            return {
+                "statusCode": 404,
+                "body": "Not Found",
+                "headers": {
+                    'Content-Type': 'text/html',
+                }
+            }
+    except:
         return {
             "statusCode": 404,
             "body": "Not Found",
@@ -59,21 +92,6 @@ def lambda_handler(event, context):
                 'Content-Type': 'text/html',
             }
         }
-    # # create a connection pool
-    # http = urllib3.PoolManager()
-    # # make a GET request to Google
-    # r = http.request('GET', 'https://google.com')
-    # # render the response HTML into user browser via API Gateway
-    # response = {
-    #     "statusCode": 200,
-    #     "body": r.data.decode('utf-8'),
-    #     "headers": {
-    #         'Content-Type': 'text/html',
-    #     }
-    # }
-    
-    
-    # return response
 
 if __name__ == "__main__":
     lambda_handler(event, context)
